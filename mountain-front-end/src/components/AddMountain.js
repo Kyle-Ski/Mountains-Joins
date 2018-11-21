37de69f5373392cd543a7496dce19963f62e5e84
Kyle Czajkowski <skiroyjenkins@gmail.com>
2018-11-20 18:26:36 -0700

    WIP: use a dropdown instead of a form to add mountains climbed

--- mountain-front-end/src/components/AddMountain.js
+++ mountain-front-end/src/components/AddMountain.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {Dropdown} from 'semantic-ui-react'
 const AddMountain = ({
         mountains, 
         handleRemoveMountain, 
@@ -13,6 +13,17 @@ const AddMountain = ({
         handleAddMountain
     }) => {
 
+        const structureDropdown = (mountain) => {
+            return mountain.map(mountain => {
+                return(
+                    {
+                        text: mountain.name,
+                        value: mountain.id
+                    }
+                )
+            })
+        }
+
         return (
             <div>
                 <button className='add-mountain' onClick={handleAddMountain}>+ Another Mountain</button>
@@ -20,13 +31,14 @@ const AddMountain = ({
                 <form onSubmit={handleSubmit} >
             {newMountains.map((mountain, idx)=>(
                 <div className='add-m-form'>
-                    <input className='input-group' onChange={handleMountainNameChange(idx)} placeholder={`Mountain ${idx + 1} Name`} value={mountain.name}/>
+                    <Dropdown placeholder='Mountain' fluid multiple search selection options={structureDropdown(mountains)} />
+                    {/* <input className='input-group' onChange={handleMountainNameChange(idx)} placeholder={`Mountain ${idx + 1} Name`} value={mountain.name}/>
                     <label>Elevation:</label>
                     <input className='input-group' onChange={handleMountainElevationChange(idx)} type='number' min='0' value={mountain.elevation}/>
                     <label>Rank:</label>
                     <input className='input-group' onChange={handleMountainRankChange(idx)} type='number' min='0' value={mountain.rank}/>
                     <input className='input-group' onChange={handleMountainRangeChange(idx)} placeholder='Mountain Range' value={mountain.range}/>
-                    <input className='input-group' onChange={handleMountainImgChange(idx)} placeholder={`Image ${idx + 1} URL`} value={mountain.imageUrl}/>
+                    <input className='input-group' onChange={handleMountainImgChange(idx)} placeholder={`Image ${idx + 1} URL`} value={mountain.imageUrl}/> */}
                     <button className='minus-mountain' onClick={handleRemoveMountain(idx)}>-</button>
                 </div>
             ))}
